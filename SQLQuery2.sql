486a0b199574925115b4f6783121819d6ac3560f
Itamarrr <itamar.oliveira@unigranrio.br>
2021-04-19 15:51:25 -0300

    script instalar o Angula

---
+++ SQLQuery2.sql
@@ -0,0 +1,7 @@
+﻿select * from PEDIDO p
+inner join CLIENTE c
+on c.IDCLIENTE = p.IDCLIENTE
+inner join ITEMPEDIDO ip
+on p.IDPEDIDO = ip.IDPEDIDO
+inner join PRODUTO pr
+on ip.IDPRODUTO = pr.IDPRODUTO
